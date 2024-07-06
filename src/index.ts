2961fd5414bcb725493eb6aa6014f3488772a8ad
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-06 21:04:47 +0200

    Add the Message class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,56 @@
 const inquirer = require('inquirer');
+const consola = require('consola');
+
+enum Action {
+  List = "list",
+  Add = "add",
+  Remove = "remove",
+  Quit = "quit"
+}
+
+type InquirerAnswers = {
+  action: Action
+}
+
+enum MessageVariant {
+  Success = "success",
+  Error = "error",
+  Info = "info",
+}
+
+class Message {
+  private content: string;
+
+  constructor(content: string) {
+    this.content = content;
+  } 
+
+  public show() {
+    console.log(this.content);
+  }
+  
+  public capitalize() {
+    this.content = this.content.charAt(0).toUpperCase() + this.content.slice(1);
+  }
+
+  public toUpperCase() {
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase() {
+    this.content = this.content.toLowerCase();
+  }
+
+  static showColorized(variant: MessageVariant, text: string): void {
+    if (variant === MessageVariant.Success) {
+        consola.success(text);
+    } else if (variant === MessageVariant.Error) {
+        consola.error(text);
+    } else if (variant === MessageVariant.Info) {
+        consola.info(text);
+    } 
+  }
+}
 
 inquirer.prompt([{
   name: 'name',
@@ -9,6 +61,6 @@ inquirer.prompt([{
   type: 'number',
   message: 'How old are you?',
   default: 18,
-}]).then((answers: { name: string, age: number}) => {
+}]).then(async (answers: { name: string, age: number}) => {
   console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
 });
