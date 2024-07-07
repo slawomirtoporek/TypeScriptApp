41c68d614d1802095cc69a40a38b6fcf51ec3288
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-07 16:01:46 +0200

    Add the UsersData class

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ class Message {
   }
   
   public capitalize() {
-    this.content = this.content.charAt(0).toUpperCase() + this.content.slice(1);
+    this.content = this.content.charAt(0).toUpperCase() + this.content.slice(1).toLocaleLowerCase();
   }
 
   public toUpperCase() {
@@ -52,15 +52,63 @@ class Message {
   }
 }
 
-inquirer.prompt([{
-  name: 'name',
-  type: 'input',
-  message: 'What\'s your name?',
-}, {
-  name: 'age',
-  type: 'number',
-  message: 'How old are you?',
-  default: 18,
-}]).then(async (answers: { name: string, age: number}) => {
-  console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-});
+const msg = new Message("heLlo world!");
+msg.show(); // "heLlo world!"
+msg.capitalize();
+msg.show(); // "Hello world!"
+msg.toLowerCase();
+msg.show(); // "hello world!"
+msg.toUpperCase();
+msg.show(); // "HELLO WORLD!"
+Message.showColorized(MessageVariant.Success, "Test"); // √ "Test"
+Message.showColorized(MessageVariant.Error, "Test 2"); // "x Test 2"
+Message.showColorized(MessageVariant.Info, "Test 3"); // ℹ "Test 3"
+
+type User = {
+  name: string;
+  age: number;
+}
+
+class UsersData {
+  private data: User[] = [];
+
+  public showAll(): void {
+    Message.showColorized(MessageVariant.Info, "Users data");
+    if (this.data.length === 0) {
+      consola.info("No data...")
+    } else {
+      console.table(this.data)
+    }
+  }
+
+  public add(user: User): void {
+    if (user.name.length > 0 && user.age > 0) {
+      this.data.push(user);
+      Message.showColorized(MessageVariant.Success, "User has been successfully added!");
+    } else {
+      Message.showColorized(MessageVariant.Error, "Wrong data!")
+    }
+  }
+
+  public remove(name: string) {
+    const index = this.data.findIndex(user => user.name === name);
+
+    if (index !== -1) {
+        this.data.splice(index, 1);
+        Message.showColorized(MessageVariant.Success, "User deleted!")
+    } else {
+      Message.showColorized(MessageVariant.Error, "User not found...");
+    }
+  }
+}
+
+const users = new UsersData();
+users.showAll();
+users.add({ name: "Jan", age: 20 });
+users.add({ name: "Adam", age: 30 });
+users.add({ name: "Kasia", age: 23 });
+users.add({ name: "Basia", age: -6 });
+users.showAll();
+users.remove("Maurycy");
+users.remove("Adam");
+users.showAll();
