58023dbd44f742da6092c3f09ff543231db0e4db
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-07 16:13:48 +0200

    Add startApp method to index.ts

--- src/index.ts
+++ src/index.ts
@@ -52,18 +52,6 @@ class Message {
   }
 }
 
-const msg = new Message("heLlo world!");
-msg.show(); // "heLlo world!"
-msg.capitalize();
-msg.show(); // "Hello world!"
-msg.toLowerCase();
-msg.show(); // "hello world!"
-msg.toUpperCase();
-msg.show(); // "HELLO WORLD!"
-Message.showColorized(MessageVariant.Success, "Test"); // √ "Test"
-Message.showColorized(MessageVariant.Error, "Test 2"); // "x Test 2"
-Message.showColorized(MessageVariant.Info, "Test 3"); // ℹ "Test 3"
-
 type User = {
   name: string;
   age: number;
@@ -103,12 +91,54 @@ class UsersData {
 }
 
 const users = new UsersData();
-users.showAll();
-users.add({ name: "Jan", age: 20 });
-users.add({ name: "Adam", age: 30 });
-users.add({ name: "Kasia", age: 23 });
-users.add({ name: "Basia", age: -6 });
-users.showAll();
-users.remove("Maurycy");
-users.remove("Adam");
-users.showAll();
+console.log("\n");
+console.info("???? Welcome to the UsersApp!");
+console.log("====================================");
+Message.showColorized(MessageVariant.Info, "Available actions");
+console.log("\n");
+console.log("list – show all users");
+console.log("add – add new user to the list");
+console.log("remove – remove user from the list");
+console.log("quit – quit the app");
+console.log("\n");
+
+const startApp = () => {
+  inquirer.prompt([{
+    name: 'action',
+    type: 'input',
+    message: 'How can I help you?',
+  }]).then(async (answers: InquirerAnswers) => {
+    switch (answers.action) {
+      case Action.List:
+        users.showAll();
+        break;
+      case Action.Add:
+        const user = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }, {
+          name: 'age',
+          type: 'number',
+          message: 'Enter age',
+        }]);
+        users.add(user);
+        break;
+      case Action.Remove:
+        const name = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }]);
+        users.remove(name.name);
+        break;
+      case Action.Quit:
+        Message.showColorized(MessageVariant.Info, "Bye bye!");
+        return;
+    }
+
+    startApp();
+  });
+}
+
+startApp();
